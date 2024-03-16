886739bc5de7e5170b074c26378044619c8f65d8
SAMI THAKUR <2022.sami.thakur@ves.ac.in>
2024-03-16 13:26:13 +0530

    arduino code

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,10 @@
+from numpy import flexible
 import streamlit as st
 from pymongo import MongoClient
 from datetime import datetime
 from dotenv import load_dotenv
 import os
+import matplotlib.pyplot as plt
 
 
 load_dotenv()
@@ -16,6 +18,8 @@ collection_names = db.list_collection_names()  # Get list of collection names
 
 
 
+
+
 # Function to fetch attendance data for a given date
 def fetch_attendance_for_date(date):
     try:
@@ -25,38 +29,87 @@ def fetch_attendance_for_date(date):
         print(f"Error fetching attendance data: {e}")
         return None
 
-# Streamlit UI
-st.title("Attendance Viewer")
 
+def count_students_status(date):
+    # Get list of all student RFID tags from 'data' collection
+    student_rfid_tags = [student["rfid"] for student in db["data"].find()]
+    print(student_rfid_tags)
+    present_count = 0
+    absent_count = 0
+    for rfid_tag in student_rfid_tags:
+        # Check if student has attendance entry for the selected date
+        attendance_entry = db[date_collection_name].find_one({"rfid_tag": rfid_tag})
+        if attendance_entry:
+            present_count += 1
+        else:
+            absent_count += 1
+    
+    return present_count, absent_count
+
+# Streamlit UI
+st.title("Dashboard")
+
+st.sidebar.title("Welcome")
 # Dropdown - Select date collection
-selected_date = st.date_input("Select a date")
+selected_date = st.sidebar.date_input("Select a date")
+
 
 # Fetch attendance data for selected date collection
 attendance_data = fetch_attendance_for_date(selected_date)
 
 # Display selected date
 st.subheader("Attendance for Date: "+ selected_date.strftime("%d-%m-%Y").replace("/", "-"))
-
 # Filter out collection names with "D6ADA_" prefix
 date_collection_name = "D6ADA_" + selected_date.strftime("%d-%m-%Y").replace("/", "-")
+
 if date_collection_name not in collection_names:
     st.write("No attendance data found for the selected date.")
     st.stop()
 
 attendance_data = fetch_attendance_for_date(date_collection_name)
 
+# Display total number of students
+total_students = db.data.count_documents({})
+
+st.divider()
+col1 , col2 = st.columns(2)
+# Count present and absent students
+with col1:
+
+    present_count, absent_count = count_students_status(selected_date.strftime("%d/%m/%Y"))
+    st.subheader("Attendance Summary:")
+    st.write(f'<div style="font-size: 17px; font-family: monospace; color: #2192FF; background-color: #31363F; margin:5px ; padding-left: 5px; padding-right: 5px; display: inline-block;">Total Student : {total_students}</div>', unsafe_allow_html=True)
+    st.write(f'<div style="font-size: 17px; font-family: monospace; color: #2192FF; background-color: #31363F; margin:5px ;padding-left: 5px; padding-right: 5px; display: inline-block;">Present Student : {present_count}</div>', unsafe_allow_html=True)
+    st.write(f'<div style="font-size: 17px; font-family: monospace; color: #2192FF; background-color: #31363F; margin:5px ;padding-left: 5px; padding-right: 5px; display: inline-block;">Absent Student : {absent_count}</div>', unsafe_allow_html=True)
+
+# Create labels and sizes for the pie chart
+labels = ['Absent', 'Present']
+sizes = [absent_count, present_count]
+
+# Create a pie chart
+fig, ax = plt.subplots(figsize=(2, 2))
+fig.set_facecolor('#B9FFF8')
+ax.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90 , colors=['#FF5733', '#33FF57'] , textprops={'fontsize': 6} , )
+ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circl
+
+with col2:
+    st.pyplot(fig)
+
+st.divider()
 # Display attendance entries
 if attendance_data:
     print(attendance_data)
-    st.write("Attendance Entries:")
+    st.subheader("Attendance Entries:")
     for entry in attendance_data:
         tag = str(entry["rfid_tag"])
         st.write('<div style="font-size: 20px; font-family: monospace;" >' + entry["name"] + " - " + entry["division"] + " - " + entry["timestamp"] + '</div>', unsafe_allow_html=True)
-        st.write(f'<div style="font-size: 20px; font-family: monospace; color: #16FF00; background-color: #31363F; padding-left: 5px; padding-right: 5px; display: inline-block;">{tag}</div>', unsafe_allow_html=True)
+        st.write(f'<div style="font-size: 18px; font-family: monospace; color: #16FF00; background-color: #31363F; padding-left: 5px; padding-right: 5px; display: inline-block;">Id: {tag}</div>', unsafe_allow_html=True)
         st.divider()
 
 else:
     st.write("No attendance entries found for the selected date.")
+
+
 
 
 
